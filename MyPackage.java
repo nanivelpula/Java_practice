277ddda3fcde4ad012508f18f05d9c5733eb8ed3
nanivelpula <107265329+nanivelpula@users.noreply.github.com>
2022-07-19 12:03:52 +0530

    Add files via upload

---
+++ MyPackage.java
@@ -0,0 +1,19 @@
+/*  JFM1T6_Assignment5:
+    Write a class "MyClass" in package "mypack" and access it from other class outside the package "mypack".
+
+    Sample Output:
+    Welcome to Packages
+*/
+
+//create a package name mypack, inside a folder called mypack, and create a class MyClas. In that create Display method and  print message
+
+//outside folder  import the mypack  and call Dislay method 
+
+package mypack;
+public class MyPackage
+  {
+    public void msg()
+    {
+      System.out.println("Welcome to package");
+    }
+  }
